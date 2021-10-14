be04415ee4a93c680ad45d332b2d6ec4820ff9a4
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-10-14 16:45:13 -0300

    Agrego clase TVProgram e instancia de cliente a MongoDB

---
+++ APIREST.MongoDB/Models/TVProgram.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIREST.MongoDB.Models
+{
+    public class TVProgram
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Driver { get; set; }
+        public string Gender { get; set; }
+        public string Schedule { get; set; }
+        public string TVChannel { get; set; }
+        public bool ThereAreGuests { get; set; }
+        public bool IsLiveBroadcast { get; set; }
+    }
+}
---
+++ APIREST.MongoDB/Repositories/MongoDBRepository.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace APIREST.MongoDB.Repositories
+{
+    public class MongoDBRepository
+    {
+        public MongoClient Client { get; set; }
+        public IMongoDatabase Database { get; set; }
+        public MongoDBRepository()
+        {
+            Client = new MongoClient("mongodb://127.0.0.1:27017");
+            Database = Client.GetDatabase("TVPrograms");
+        }
+    }
+}
