f337874abea8cccd563a78e21a8d8259d56a90a0
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-10-14 16:56:35 -0300

    Generamos la interface ITVProgramCollection

---
+++ APIREST.MongoDB/Repositories/ITVProgramCollection.cs
@@ -0,0 +1,18 @@
+﻿using APIREST.MongoDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREST.MongoDB.Repositories
+{
+    public interface ITVProgramCollection
+    {
+        Task InsertTVProgram(TVProgram program);
+        Task UpdateTVProgram(TVProgram program);
+        Task DeleteTVProgram(string id);
+        Task<List<TVProgram>> GetAllTVPrograms();
+        Task<TVProgram> GetTVProgramById(string id);
+
+    }
+}
---
+++ APIREST.MongoDB/Repositories/TVProgramCollection.cs
@@ -0,0 +1,36 @@
+﻿using APIREST.MongoDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREST.MongoDB.Repositories
+{
+    public class TVProgramCollection : ITVProgramCollection
+    {
+        public Task DeleteTVProgram(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TVProgram>> GetAllTVPrograms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TVProgram> GetTVProgramById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertTVProgram(TVProgram program)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTVProgram(TVProgram program)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
